97e3a2044e90ed18923d2b3c4ba01d63522aaeee
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-06-06 18:31:17 -0300

    Renomeação nome das classes

--- ChequePorExtenso.Test/Testes.cs
+++ ChequePorExtenso.Test/Testes.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ChequePorExtenso.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class Testes
     {
         Actions conversor = new Actions();
 

f81e05c23014ce49de6054fbeea4e946a040f631
Joao Pedro T. Correa <joaopedrotcorrea1212@gmail.com>
2022-06-06 18:28:53 -0300

    Add project files.

---
+++ ChequePorExtenso.Test/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChequePorExtenso.Test
+{
+    public class Actions
+    {
+        Dictionary<int, string> numeros = new Dictionary<int, string>();
+
+
+        public Actions()
+        {
+            numeros.Add(1, "Um");
+            numeros.Add(2, "Dois");
+            numeros.Add(3, "Três");
+            numeros.Add(4, "Quatro");
+            numeros.Add(5, "Cinco");
+            numeros.Add(6, "Seis");
+            numeros.Add(7, "Sete");
+            numeros.Add(8, "Oito");
+            numeros.Add(9, "Nove");
+            numeros.Add(10, "Dez");
+            numeros.Add(11, "Onze");
+            numeros.Add(12, "Doze");
+            numeros.Add(13, "Treze");
+            numeros.Add(14, "Quatorze");
+            numeros.Add(15, "Quinze");
+            numeros.Add(16, "Dezesseis");
+            numeros.Add(17, "Dezessete");
+            numeros.Add(18, "Dezoito");
+            numeros.Add(19, "Dezenove");
+            numeros.Add(20, "Vinte");
+            numeros.Add(30, "Trinta");
+            numeros.Add(40, "Quarenta");
+            numeros.Add(50, "Cinquenta");
+            numeros.Add(60, "Sessenta");
+            numeros.Add(70, "Setenta");
+            numeros.Add(80, "Oitenta");
+            numeros.Add(90, "Noventa");
+            numeros.Add(100, "Cento");
+            numeros.Add(200, "Duzentos");
+            numeros.Add(300, "Trezentos");
+            numeros.Add(400, "Quatrocentos");
+            numeros.Add(500, "Quinhentos");
+            numeros.Add(600, "Seiscentos");
+            numeros.Add(700, "Setecentos");
+            numeros.Add(800, "Oitocentos");
+            numeros.Add(900, "Novecentos");
+
+
+
+        }
+
+
+        public string Converter(int numero)
+        {
+            string numeroPorExtenso = "";
+            string numeroString = numero.ToString();
+
+
+
+            if (numero == 100)
+                return "Cem";
+
+            if (numero > 100)
+            {
+                //Pega centena
+                int centena = Int32.Parse(numeroString.Substring(0, 1));
+                centena = centena * 100;
+
+                numeroPorExtenso += numeros[centena];
+
+                numero -= centena;
+
+                numeroPorExtenso += " e ";
+                
+                numeroString = numero.ToString();
+            }
+            //Pega Dezena
+
+            if (numero > 20)
+            {
+
+                int dezena = Int32.Parse(numeroString.Substring(0, 1));
+                dezena = dezena * 10;
+
+                if (dezena != 0)
+                {
+   
+                    numeroPorExtenso += numeros[dezena];
+
+                    numero -= dezena;
+                }
+
+
+                numeroPorExtenso += " e ";
+            }
+                numeroPorExtenso += numeros[numero];
+            
+
+
+                return numeroPorExtenso;
+            
+        }
+    }
+}
---
+++ ChequePorExtenso.Test/UnitTest1.cs
@@ -0,0 +1,78 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChequePorExtenso.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Actions conversor = new Actions();
+
+        [TestMethod]
+        [DataRow(1, "Um")]
+        [DataRow(2, "Dois")]
+        [DataRow(3, "Três")]
+        [DataRow(4, "Quatro")]
+        [DataRow(5, "Cinco")]
+        [DataRow(6, "Seis")]
+        [DataRow(7, "Sete")]
+        [DataRow(8, "Oito")]
+        [DataRow(9, "Nove")]
+        [DataRow(10, "Dez")]
+        
+        [DataRow(11, "Onze")]
+        [DataRow(12, "Doze")]
+        [DataRow(13, "Treze")]
+        [DataRow(14, "Quatorze")]
+        [DataRow(15, "Quinze")]
+        [DataRow(16, "Dezesseis")]
+        [DataRow(17, "Dezessete")]
+        [DataRow(18, "Dezoito")]
+        [DataRow(19, "Dezenove")]
+        [DataRow(20, "Vinte")]
+
+        [DataRow(21, "Vinte e Um")]
+        [DataRow(22, "Vinte e Dois")]
+        [DataRow(23, "Vinte e Três")]
+        [DataRow(24, "Vinte e Quatro")]
+        [DataRow(25, "Vinte e Cinco")]
+        [DataRow(26, "Vinte e Seis")]
+
+        [DataRow(36, "Trinta e Seis")]
+        [DataRow(55, "Cinquenta e Cinco")]
+        [DataRow(78, "Setenta e Oito")]
+        [DataRow(99, "Noventa e Nove")]
+
+
+        public void Converter_Unidades_E_Dezenas(int numero, string resultadoEsperado)
+        {
+            //Arrange
+            int numeroParaConversao = numero;
+
+            //Action
+            var resultadoConversao = conversor.Converter(numeroParaConversao);
+            
+            //Assert
+
+            Assert.AreEqual(resultadoEsperado, resultadoConversao);
+        }
+
+        [TestMethod]
+        [DataRow(105, "Cento e Cinco")]
+        [DataRow(155, "Cento e Cinquenta e Cinco")]
+        [DataRow(148, "Cento e Quarenta e Oito")]
+        [DataRow(169, "Cento e Sessenta e Nove")]
+
+        public void Converter_Centenas(int numero, string resultadoEsperado)
+        {
+            //Arrange
+            int numeroParaConversao = numero;
+
+            //Action
+            var resultadoConversao = conversor.Converter(numeroParaConversao);
+
+            //Assert
+
+            Assert.AreEqual(resultadoEsperado, resultadoConversao);
+        }
+    }
+}
